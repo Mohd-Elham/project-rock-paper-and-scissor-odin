3ac8938d6895d05d671fc11edb80720ac80c81a0
Mohd-Elham <muhammedelham@gmail.com>
2023-08-14 00:14:31 +0530

    ver final

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-let user_sel = prompt("Enter your choice");
-let computer_sel = Math.floor(Math.random()*3)+1;
 
-console.log(user_sel);
-console.log(computer_sel);
-//let rock=1, scissor=2 and paper=3
-if(user_sel==computer_sel){
-    alert("tie");
+function myRock(){
+    var user_sel =1;
+    test(user_sel);
 }
-else if((user_sel==1 && computer_sel==2)||(user_sel==2 && computer_sel==3)||(user_sel==3 && computer_sel==1)){
-    alert("you win ");
+function myPaper(){
+    var user_sel =3;
+    test(user_sel);
 }
-// else if(){
-//     alert("you win ");
-// }
-// else if(user_sel==3 && computer_sel==1){
-//     alert("you win ");
-// }
-else{
-    alert("you lose");
+function myScissor(){
+    var user_sel =2;
+    test(user_sel);
+}
+
+var myDivr = document.getElementById('rock');
+var myDivs = document.getElementById('scissor');
+var myDivp = document.getElementById('paper');
+var myresult = document.getElementById("rslt");
+var cChoice = document.getElementById("cmptr");
+
+myDivr.addEventListener('click',myRock);
+myDivs.addEventListener('click',myScissor);
+myDivp.addEventListener('click',myPaper);
+
+function test(user_sel){
+    let computer_sel = Math.floor(Math.random()*3)+1;
+
+    switch(computer_sel){
+        case 1: cChoice.innerHTML="Computers Selection: rock";break;
+
+        case 3: cChoice.innerHTML="Computers Selection: paper";break;
+
+        case 2: cChoice.innerHTML="Computers Selection: scissor";break;
+
+        default: break;
+    }
+    if(user_sel==computer_sel){
+        myresult.innerHTML="tie";
+    }
+
+    else if((user_sel==1 && computer_sel==2)||(user_sel==2 && computer_sel==3)||(user_sel==3 && computer_sel==1)){
+        myresult.innerHTML="You win";
+    }
+
+    else if((user_sel==2 && computer_sel==1)||(user_sel==3 && computer_sel==2)||(user_sel==1 && computer_sel==3)){
+        myresult.innerHTML=" you lose";
+        
+    }
 }
